ce875b412edba6dac2999052c860f9d0277fb0f8
Kyler Nguyen <tkien2703@gmail.com>
2025-04-02 11:01:46 +0700

    chore(hardhat): add espresso rollup network

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -28,5 +28,11 @@ module.exports = {
       accounts: [process.env.PRIVATE_KEY, process.env.SECOND_PRIVATE_KEY],
       explorer: "https://explorer.abc.t.raas.gelato.cloud/",
     },
+    espressoRollup: {
+      url: "http://13.239.65.206:8547",
+      chainId: 42169,
+      accounts: [process.env.PRIVATE_KEY, process.env.SECOND_PRIVATE_KEY],
+      explorer: "https://explorer.decaf.testnet.espresso.network/",
+    }
   },
 };
